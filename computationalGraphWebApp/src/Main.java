23e2916104df213168ed5b75e3422f6a8b8f312e
Ahigad Genish <ahigad.genish@gmail.com>
2024-07-27 13:09:30 +0300

    Update Main.java

--- computationalGraphWebApp/src/Main.java
+++ computationalGraphWebApp/src/Main.java
@@ -1,5 +1,8 @@
-import server.MainTrain;
+import server.*;
 
+/// Advanced Programming exercise 56
+/// Student Names: Ahigad Genish & Omer Sela
+/// ID : 31628022 & 316539535
 public class Main {
     public static void main(String[] args) {
         MainTrain.main(args);
